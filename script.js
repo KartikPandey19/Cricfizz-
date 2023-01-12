0149115d5d81d0ffb5d9f900c3b3389933fc5dd5
Kartik Pandey <kartik2000pandey@gmail.com>
2023-01-12 17:44:16 +0530

    slide bar inprogress

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ let searchForm = document.querySelector('.search-form');
 document.querySelector('#search-btn').onclick =() =>{
     searchForm.classList.toggle('active');
 }
+
+var swiper = new Swiper(".home-slider", {
+    slidesPerView: 5,
+    spaceBetween: 30,
+    freeMode: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+ 
